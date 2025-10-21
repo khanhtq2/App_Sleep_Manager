10f335b2297c6c9366de519870df740b43d2ab34
Trần Quốc Khánh <khanhtqn123@gmail.com>
2025-10-21 16:21:29 +0700

    screen_splash

--- lib/views/screen_splash.dart
+++ lib/views/screen_splash.dart
@@ -2,15 +2,47 @@ import 'package:flutter/material.dart';
 
 class ScreenSplash extends StatelessWidget {
   const ScreenSplash({super.key});
+
   @override
   Widget build(BuildContext context) {
+    double logoHeight = 150;
+    const String urlLogo = 'assets/img_project/Star.png';
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Color(0xFF8ED6FF),
-        child: Image.asset('assets/img_project/mail.png'),
-        
+      backgroundColor: const Color(0xFF8ED6FF),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 70,
+            left: 40,
+            child: Image.asset(urlLogo, height: logoHeight),
+          ),
+          Positioned(
+            top: 100,
+            right: 10,
+            child: Image.asset(urlLogo, height: logoHeight),
+          ),
+          Positioned(
+            bottom: 100,
+            right: 10,
+            child: Image.asset(urlLogo, height: logoHeight),
+          ),
+          Positioned(
+            bottom: 130,
+            left: 0,
+            child: Image.asset(urlLogo, height: logoHeight),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 120,
+            child: Image.asset(urlLogo, height: logoHeight),
+          ),
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(400),
+              child: Image.asset('assets/img_project/logo.jpg'),
+            ),
+          ),
+        ],
       ),
     );
   }
